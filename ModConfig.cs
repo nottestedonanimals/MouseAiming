f9183f9c043469f15fb14cd444fb5cb8eab5e252
nottestedonanimals <nottestedonanimals207@gmail.com>
2024-04-14 12:21:32 -0400

    add configuration option for determining behavior of scythes

---
+++ ModConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MouseAiming {
+    internal class ModConfig {
+
+        public bool includeScythe = false;
+
+    }
+}
--- ModEntry.cs
+++ ModEntry.cs
@@ -9,7 +9,16 @@ namespace MouseAiming {
 
     internal sealed class ModEntry : Mod {
 
+        private ModConfig Config;
+
+        public const string scytheId = "47";
+        public const string goldenScytheId = "53";
+        public const string iridiumScytheID = "66";
+
         public override void Entry(IModHelper helper) {
+            
+            this.Config = this.Helper.ReadConfig<ModConfig>();
+
             helper.Events.Input.ButtonsChanged += this.OnButtonChange;
         }
 
@@ -20,7 +29,6 @@ namespace MouseAiming {
             // 1 = Right
             // 2 = Down
             // 3 = Left
-            // Game1.player.faceDirection(3);
 
             if (!Context.IsWorldReady)
                 return;
@@ -29,39 +37,42 @@ namespace MouseAiming {
             SButton actionButton = new InputButton(true).ToSButton();
             ICursorPosition cursorPosition = this.Helper.Input.GetCursorPosition();
 
-            if (actionButton.IsUseToolButton() && this.Helper.Input.GetState(actionButton) == SButtonState.Pressed && Game1.activeClickableMenu == null
-                && player.CurrentTool is StardewValley.Tools.MeleeWeapon && !Game1.eventUp && player.canMove && Game1.player.ActiveObject == null &&
-                !Game1.player.isEating && Game1.player.CurrentTool != null) {
+            
 
+            if (actionButton.IsUseToolButton() && this.Helper.Input.GetState(actionButton) == SButtonState.Pressed && Game1.activeClickableMenu == null
+            && player.CurrentTool is StardewValley.Tools.MeleeWeapon && !Game1.eventUp && player.canMove && Game1.player.ActiveObject == null &&
+            !Game1.player.isEating && Game1.player.CurrentTool != null) {
 
                 Vector2 playerStanding = player.getStandingPosition();
 
                 float rise = cursorPosition.AbsolutePixels.Y - playerStanding.Y;
                 float run = cursorPosition.AbsolutePixels.X - playerStanding.X;
-                float slope = rise/run;
+                float slope = rise / run;
 
-                if (((slope > 1 || slope < -1) && run < 0 && rise < 0) || ((slope < -1) && run > 0 && rise < 0) || (slope > 1 && run > 0 && rise < 0)) {
+                if ((!this.Config.includeScythe) && ((Game1.player.CurrentTool.ItemId == "47") || (Game1.player.CurrentTool.ItemId == "53") || (Game1.player.CurrentTool.ItemId == "66"))) {
 
-                    player.faceDirection(0);
+                } else {
 
-                } else if ((slope > -1 && run > 0 && rise < 0) || (slope < 1 && run > 0 && rise > 0)) {
+                    if (((slope > 1 || slope < -1) && run < 0 && rise < 0) || ((slope < -1) && run > 0 && rise < 0) || (slope > 1 && run > 0 && rise < 0)) {
 
-                    player.faceDirection(1);
+                        player.faceDirection(0);
 
-                }
-                
-                else if (((slope > 1 || slope < -1) && run > 0 && rise > 0) || ((slope < -1) && run < 0 && rise > 0) || (slope > 1 && run > 0 && rise < 0)) {
+                    } else if ((slope > -1 && run > 0 && rise < 0) || (slope < 1 && run > 0 && rise > 0)) {
 
-                    player.faceDirection(2);
+                        player.faceDirection(1);
 
-                } else if ((slope > -1 && run < 0 && rise > 0) || (slope < 1 && run < 0 && rise < 0)) {
+                    } else if (((slope > 1 || slope < -1) && run > 0 && rise > 0) || ((slope < -1) && run < 0 && rise > 0) || (slope > 1 && run > 0 && rise < 0)) {
 
-                    player.faceDirection(3);
+                        player.faceDirection(2);
 
-                }
-               
-                else {
-                    player.faceDirection(player.FacingDirection);
+                    } else if ((slope > -1 && run < 0 && rise > 0) || (slope < 1 && run < 0 && rise < 0)) {
+
+                        player.faceDirection(3);
+
+                    } else {
+                        player.faceDirection(player.FacingDirection);
+                    }
+
                 }
 
             }
